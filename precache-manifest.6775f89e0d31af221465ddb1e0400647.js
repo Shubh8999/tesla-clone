dbda9aa9206677c77a2e843022b26d4f95a43329
Shubham <2020.shubham.kamodkar@ves.ac.in>
2023-02-20 19:35:42 +0530

    Updates

---
+++ precache-manifest.6775f89e0d31af221465ddb1e0400647.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "64221002e1d67bba497a",
+    "url": "/tesla-clone/static/css/main.2f5d28fe.chunk.css"
+  },
+  {
+    "revision": "64221002e1d67bba497a",
+    "url": "/tesla-clone/static/js/main.64221002.chunk.js"
+  },
+  {
+    "revision": "e67e65479d4f06baac99",
+    "url": "/tesla-clone/static/js/1.e67e6547.chunk.js"
+  },
+  {
+    "revision": "5a45374efe6ff165c29e",
+    "url": "/tesla-clone/static/js/2.5a45374e.chunk.js"
+  },
+  {
+    "revision": "f74ab48c562f9ab31670",
+    "url": "/tesla-clone/static/js/runtime~main.f74ab48c.js"
+  },
+  {
+    "revision": "be6228998e31904c0d42d148597680fa",
+    "url": "/tesla-clone/static/media/logo.be622899.svg"
+  },
+  {
+    "revision": "f0d7822966c6c667eb27cc2dec277cd7",
+    "url": "/tesla-clone/index.html"
+  }
+];
